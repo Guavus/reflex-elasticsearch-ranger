fc95d69777f09eb945c5ead3865b3420094fcc1d
rajat.goel <rajat.goel@guavus.com>
2018-12-21 17:07:27 +0530

    ARCH-137: Adding tag based support for es

---
+++ ranger-tagsync-elasticsearch/src/main/java/org/apache/ranger/tagsync/source/atlas/AtlasESResourceMapper.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.ranger.tagsync.source.atlas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.atlas.model.instance.AtlasEntityHeader;
+import org.apache.atlas.typesystem.IReferenceableInstance;
+import org.apache.commons.lang.StringUtils;
+import org.apache.ranger.plugin.model.RangerPolicy;
+import org.apache.ranger.plugin.model.RangerPolicy.RangerPolicyResource;
+import org.apache.ranger.plugin.model.RangerServiceResource;
+
+public class AtlasESResourceMapper extends AtlasResourceMapper {
+	public static final String ENTITY_TYPE_ES_INDEX = "es_index";
+	public static final String RANGER_TYPE_ES_INDEX = "index";
+
+	public static final String ENTITY_ATTRIBUTE_QUALIFIED_NAME = "qualifiedName";
+
+	public static final String[] SUPPORTED_ENTITY_TYPES = { ENTITY_TYPE_ES_INDEX };
+
+	public AtlasESResourceMapper() {
+		super("elasticsearch", SUPPORTED_ENTITY_TYPES);
+	}
+
+	@Override
+	public RangerServiceResource buildResource(final IReferenceableInstance entity) throws Exception {
+		String entityGuid    = entity.getId() != null ? entity.getId()._getId() : null;
+		String qualifiedName = getEntityAttribute(entity, ENTITY_ATTRIBUTE_QUALIFIED_NAME, String.class);
+
+		return getServiceResource(entityGuid, qualifiedName);
+	}
+
+	@Override
+	public RangerServiceResource buildResource(final AtlasEntityHeader entity) throws Exception {
+		String entityGuid    = entity.getGuid();
+		String qualifiedName = getEntityAttribute(entity, ENTITY_ATTRIBUTE_QUALIFIED_NAME, String.class);
+
+		return getServiceResource(entityGuid, qualifiedName);
+	}
+
+	private RangerServiceResource getServiceResource(String entityGuid, String qualifiedName) throws Exception {
+		String index = getResourceNameFromQualifiedName(qualifiedName);
+
+		if(StringUtils.isEmpty(index)) {
+			throwExceptionWithMessage("index not found in attribute '" + ENTITY_ATTRIBUTE_QUALIFIED_NAME +  "'");
+		}
+
+		String clusterName = getClusterNameFromQualifiedName(qualifiedName);
+
+		if(StringUtils.isEmpty(clusterName)) {
+			clusterName = defaultClusterName;
+		}
+
+		if(StringUtils.isEmpty(clusterName)) {
+			throwExceptionWithMessage("attribute '" + ENTITY_ATTRIBUTE_QUALIFIED_NAME +  "' not found in entity");
+		}
+
+
+		Map<String, RangerPolicyResource> elements = new HashMap<String, RangerPolicy.RangerPolicyResource>();
+		Boolean isExcludes  = Boolean.FALSE;
+		Boolean isRecursive = Boolean.FALSE;
+
+		elements.put(RANGER_TYPE_ES_INDEX, new RangerPolicyResource(index, isExcludes, isRecursive));
+
+		String  serviceName = getRangerServiceName(clusterName);
+
+		return new RangerServiceResource(entityGuid, serviceName, elements);
+	}
+}
