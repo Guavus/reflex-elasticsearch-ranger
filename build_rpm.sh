a5fd26ad742b11e16437f52a9a0cdc3d6f8e2599
Bipin <bipin.soni@thalesgroup.com>
2020-07-26 22:00:44 +0530

    Revert "Updated build_rpm"
    This reverts commit addd4a8ee9b5ed86d7df78b9cf2ee0b75d9e50fc.

--- build_rpm.sh
+++ build_rpm.sh
@@ -27,22 +27,18 @@ fi
  mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_ES_SERVICE_RPM_BASE_PATH}
  mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_ES_SERVICE_RPM_BASE_PATH}/lib
 
- mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}
- mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}/lib
-
  cp -r ./ranger-elasticsearch-service/conf ${TEMP_PACKAGE_DIR}/${RANGER_ES_SERVICE_RPM_BASE_PATH}
  cp -r ./ranger-elasticsearch-service/target/*.jar ${TEMP_PACKAGE_DIR}/${RANGER_ES_SERVICE_RPM_BASE_PATH}/lib
  cp -r ./ranger-tagsync-elasticsearch/target/*.jar ${TEMP_PACKAGE_DIR}/${RANGER_ES_SERVICE_RPM_BASE_PATH}/lib
- cp -r ./ranger-privileges-evaluator/target/*.jar ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}/lib
 
  fpm -f -s dir -t rpm --rpm-os linux -v ${VERSION} --iteration ${REL} --chdir $TEMP_PACKAGE_DIR -p $RANGER_ES_SERVICED_DIST_DIR_PLUGIN -n $RANGER_ES_SERVICE_RPM_NAME .
 
  echo "###### END: RPM CREATION FOR RANGER ES PLUGIN ######"
 
  echo -e "# # # # # # # START : Creating RPM package for ranger privilege evaluator # # # # # # #"
-#  mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}
-#  mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}/lib
-#  cp -r ./ranger-privileges-evaluator/target/*.jar ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}/lib
+ mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}
+ mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}/lib
+ cp -r ./ranger-privileges-evaluator/target/*.jar ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}/lib
 
  fpm -f -s dir -t rpm --rpm-os linux -v ${VERSION} --iteration ${REL} --chdir $TEMP_PACKAGE_DIR -p $RANGER_PRIVILEGE_EVALUATOR_DIST_DIR_PLUGIN -n $RANGER_PRIVILEGE_EVALUATOR_RPM_NAME .
  echo "###### END: RPM CREATION FOR RANGER PRIVILEGE EVALUATOR ######"
