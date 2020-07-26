1df8e3a684fdc0ebad5e0646ac28490071a621ca
Bipin <bipin.soni@thalesgroup.com>
2020-07-26 12:15:14 +0530

    Created two separate rpm, one for es ranger plugin and another for ranger privilege evaluator

--- build_rpm.sh
+++ build_rpm.sh
@@ -3,10 +3,13 @@ set -e
 pushd "$(dirname "$0")"
 
 TEMP_PACKAGE_DIR=`pwd -P`"/.package"    #"${TEMP_RPM_DIR}/package"
-RPM_BASE_PATH="/opt/guavus/ranger-es/"
-DIST_DIR_PLUGIN="./dist/rangeres"
-DIST_DIR_INSTALLER="./dist/installer"
-RPM_NAME="guavus-ranger-es-plugin"
+RANGER_ES_SERVICE_RPM_BASE_PATH="/opt/guavus/ranger-es-service/"
+RANGER_ES_SERVICED_DIST_DIR_PLUGIN="./dist/ranger-es-service"
+RANGER_ES_SERVICE_RPM_NAME="guavus-ranger-es-plugin"
+
+RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH="/opt/guavus/ranger-privilege-evaluator/"
+RANGER_PRIVILEGE_EVALUATOR_DIST_DIR_PLUGIN="./dist/ranger-privilege-evaluator"
+RANGER_PRIVILEGE_EVALUATOR_RPM_NAME="guavus-ranger-privilege-evaluator"
 
 VERSION=$1
 REL=$2
@@ -18,17 +21,27 @@ then
 fi
 
  echo "###### START: RPM CREATION FOR RANGER ES PLUGIN ######"
- echo -e "# # # # # # # START : Creating RPM package # # # # # # #"
-    #cleanup
+ echo -e "# # # # # # # START : Creating RPM package for ranger plugin # # # # # # #"
+ #cleanup
  rm -rf ${TEMP_PACKAGE_DIR}/*
- mkdir -p ${TEMP_PACKAGE_DIR}/${RPM_BASE_PATH}
- mkdir -p ${TEMP_PACKAGE_DIR}/${RPM_BASE_PATH}/lib
- cp -r ./ranger-elasticsearch-service/conf ${TEMP_PACKAGE_DIR}/${RPM_BASE_PATH}
- cp -r ./ranger-elasticsearch-service/target/*.jar ${TEMP_PACKAGE_DIR}/${RPM_BASE_PATH}/lib
- cp -r ./ranger-tagsync-elasticsearch/target/*.jar ${TEMP_PACKAGE_DIR}/${RPM_BASE_PATH}/lib
- cp -r ./ranger-privileges-evaluator/target/*.jar ${TEMP_PACKAGE_DIR}/${RPM_BASE_PATH}/lib
- fpm -f -s dir -t rpm --rpm-os linux -v ${VERSION} --iteration ${REL} --chdir $TEMP_PACKAGE_DIR -p $DIST_DIR_PLUGIN -n $RPM_NAME .
+ mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_ES_SERVICE_RPM_BASE_PATH}
+ mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_ES_SERVICE_RPM_BASE_PATH}/lib
+
+ cp -r ./ranger-elasticsearch-service/conf ${TEMP_PACKAGE_DIR}/${RANGER_ES_SERVICE_RPM_BASE_PATH}
+ cp -r ./ranger-elasticsearch-service/target/*.jar ${TEMP_PACKAGE_DIR}/${RANGER_ES_SERVICE_RPM_BASE_PATH}/lib
+ cp -r ./ranger-tagsync-elasticsearch/target/*.jar ${TEMP_PACKAGE_DIR}/${RANGER_ES_SERVICE_RPM_BASE_PATH}/lib
+
+ fpm -f -s dir -t rpm --rpm-os linux -v ${VERSION} --iteration ${REL} --chdir $TEMP_PACKAGE_DIR -p $RANGER_ES_SERVICED_DIST_DIR_PLUGIN -n $RANGER_ES_SERVICE_RPM_NAME .
+
  echo "###### END: RPM CREATION FOR RANGER ES PLUGIN ######"
+
+ echo -e "# # # # # # # START : Creating RPM package for ranger privilege evaluator # # # # # # #"
+ mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}
+ mkdir -p ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}/lib
+ cp -r ./ranger-privileges-evaluator/target/*.jar ${TEMP_PACKAGE_DIR}/${RANGER_PRIVILEGE_EVALUATOR_RPM_BASE_PATH}/lib
+
+ fpm -f -s dir -t rpm --rpm-os linux -v ${VERSION} --iteration ${REL} --chdir $TEMP_PACKAGE_DIR -p $RANGER_PRIVILEGE_EVALUATOR_DIST_DIR_PLUGIN -n $RANGER_PRIVILEGE_EVALUATOR_RPM_NAME .
+ echo "###### END: RPM CREATION FOR RANGER PRIVILEGE EVALUATOR ######"
 
  rm -rf ${TEMP_PACKAGE_DIR}
 
