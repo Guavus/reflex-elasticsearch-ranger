f968fa77c5367f5aa99753ce60f88c34c2b39bb4
rajat.goel <rajat.goel@thalesgroup.com>
2020-08-14 15:18:34 +0530

    RAF-2219: Adding changes for tag based policy support

--- ranger-tagsync-elasticsearch/src/main/java/com/guavus/tagsync/source/atlas/AtlasESResourceMapper.java
+++ ranger-tagsync-elasticsearch/src/main/java/com/guavus/tagsync/source/atlas/AtlasESResourceMapper.java
@@ -1,33 +1,20 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-package org.apache.ranger.tagsync.source.atlas;
+package com.guavus.tagsync.source.atlas;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.atlas.model.instance.AtlasEntityHeader;
-import org.apache.atlas.typesystem.IReferenceableInstance;
 import org.apache.commons.lang.StringUtils;
+import org.apache.ranger.tagsync.source.atlas.AtlasResourceMapper;
 import org.apache.ranger.plugin.model.RangerPolicy;
 import org.apache.ranger.plugin.model.RangerPolicy.RangerPolicyResource;
 import org.apache.ranger.plugin.model.RangerServiceResource;
+import org.apache.ranger.tagsync.source.atlasrest.RangerAtlasEntity;
+
+/*
+ * Provides mapping of Atlas ES entity to Ranger ES entity for Tag based policies
+
+ * @author rajat.goel
+ */
 
 public class AtlasESResourceMapper extends AtlasResourceMapper {
 	public static final String ENTITY_TYPE_ES_INDEX = "es_index";
@@ -42,21 +29,12 @@ public class AtlasESResourceMapper extends AtlasResourceMapper {
 	}
 
 	@Override
-	public RangerServiceResource buildResource(final IReferenceableInstance entity) throws Exception {
-		String entityGuid    = entity.getId() != null ? entity.getId()._getId() : null;
-		String qualifiedName = getEntityAttribute(entity, ENTITY_ATTRIBUTE_QUALIFIED_NAME, String.class);
+	public RangerServiceResource buildResource(final RangerAtlasEntity entity) throws Exception {
+		String entityGuid    = entity.getGuid() != null ? entity.getGuid() : null;
+		String qualifiedName = (String)entity.getAttributes().get(AtlasResourceMapper.ENTITY_ATTRIBUTE_QUALIFIED_NAME);
 
 		return getServiceResource(entityGuid, qualifiedName);
 	}
-
-	/*
-	public RangerServiceResource buildResource(final AtlasEntityHeader entity) throws Exception {
-		String entityGuid    = entity.getGuid();
-		String qualifiedName = getEntityAttribute(entity, ENTITY_ATTRIBUTE_QUALIFIED_NAME, String.class);
-
-		return getServiceResource(entityGuid, qualifiedName);
-	}
-	*/
 
 	private RangerServiceResource getServiceResource(String entityGuid, String qualifiedName) throws Exception {
 		String index = getResourceNameFromQualifiedName(qualifiedName);
